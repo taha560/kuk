88a98cd3e542ac89daace74a03da76d4a7d5b1ee
ADVAN <mahdijanlou@gmail.com>
2016-05-25 13:41:30 +0430

    semat plugins by @janlou

--- plugins/semat.lua
+++ plugins/semat.lua
@@ -2,20 +2,20 @@
 
 local function run(msg, matches)
   if matches[1]:lower() == 'semat' and not is_momod(msg) then
-     send_document(get_receiver(msg), "./semat/member.webp", ok_cb, false)
-    return 'You are a member ًں‘¶'
+     send_document(get_receiver(msg), "./system/semat/member.webp", ok_cb, false)
+    return 'You are a member'
   end
    if matches[1]:lower() == 'semat' and is_sudo(msg) then
-     send_document(get_receiver(msg), "./semat/sudo.webp", ok_cb, false)
-    return 'You are my sudo ًںکٹ'
+     send_document(get_receiver(msg), "./system/semat/sudo.webp", ok_cb, false)
+    return 'You are my sudo'
    end
     if matches[1]:lower() == 'semat' and is_owner(msg) then
-     send_document(get_receiver(msg), "./semat/owner.webp", ok_cb, false)
-    return 'You are a owner ًں—£'
+     send_document(get_receiver(msg), "./system/semat/owner.webp", ok_cb, false)
+    return 'You are a owner'
    end
   if matches[1]:lower() == 'semat' and is_momod(msg) then
-     send_document(get_receiver(msg), "./semat/momod.webp", ok_cb, false)
-    return 'You are a admin ًں‘¤'
+     send_document(get_receiver(msg), "./system/semat/momod.webp", ok_cb, false)
+    return 'You are a admin'
   end
 end
 return {
