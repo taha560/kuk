3faa0e5cf705fdba4043ec8a2f5cc6c1a747bc4a
ADVAN <mahdijanlou@gmail.com>
2016-05-26 12:32:23 +0430

    helps for bot

---
+++ helps/HelpSuper.lua
@@ -0,0 +1,16 @@
+function help(msg, matches)
+ if is_super_group(msg) then
+    if matches[1] == 'help' then
+	 return [[
+	 
+	 
+	 
+	]]
+	end
+ end
+end
+return {
+advan = "http://github.com/janlou",
+patterns = {"^(help)$"},
+help = run,
+}
