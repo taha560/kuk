f4161d2e644a50e2182c9ec37b85067692e895ce
ADVAN <mahdijanlou@gmail.com>
2016-05-26 12:31:12 +0430

    Create about.lua

---
+++ adv/version/about.lua
@@ -0,0 +1,18 @@
+function about(msg, matches)
+  return [[
+  about text:
+  
+  Channel: @AdvanTM
+  
+  Sudo users:
+  @janlou
+  @amiraliiiiiiiii81
+  @advan_sudo
+  
+  ]]
+end
+return {
+advan = "http://github.com/janlou",
+patterns = {"^([Aa]bout)$"},
+about = run,
+}
